e241f136bfdd879bc2355ef772d20b7f87041400
ChengHsin0922 <yoyo12825@gmail.com>
2024-06-13 11:31:13 +0800

    202406131131

--- vincent41.php
+++ vincent41.php
@@ -1,10 +1,19 @@
 <?php
     $mysqli = new mysqli('localhost', 'root', '', 'iii');
     $mysqli->set_charset('utf8');
-    $sql = 'SELECT * FROM gift';
+    $sql = 'SELECT id, name, addr FROM gift';
     $result = $mysqli->query($sql);
-    $row = $result->fetch_array();
-    var_dump($row);
+    $row = $result->fetch_object();
+    // var_dump($row);
+    // while($row = $result->fetch_array()) {
+    //     echo "{$row['id']} : {$row['name']}<br />";
+    // }
+    while($row = $result->fetch_object()) {
+        echo "{$row->id} : {$row->name} : {$row->addr}<br />";
+    }
+
+
+
 ?>
 
-<!-- fetch每次只會抓一筆資料(一個row) -->
+<!-- fetch每次只會抓一筆資料(一個 row或是改成物件obj) -->
