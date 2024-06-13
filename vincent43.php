8dfb0f6a72954c551c165127bb9146bccff102d3
ChengHsin0922 <yoyo12825@gmail.com>
2024-06-13 12:08:28 +0800

    202406131208

---
+++ vincent43.php
@@ -0,0 +1,11 @@
+<?php 
+    $passwd = '123456';
+    $newpasswd = password_hash($passwd, PASSWORD_DEFAULT);
+    echo $newpasswd;
+    if (password_verify('123456', $newpasswd)) {
+        echo 'ok';
+    }else {
+        echo 'XX';
+    }
+
+?>
