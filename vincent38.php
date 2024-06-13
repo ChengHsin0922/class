4b7d4883024354b7204add30e9eaf6615d8f4a91
ChengHsin0922 <yoyo12825@gmail.com>
2024-06-13 09:40:37 +0800

    202406130940

---
+++ vincent38.php
@@ -0,0 +1,17 @@
+<?php
+    $mysqli = new mysqli('localhost', 'root', '', 'iii');
+    $mysqli->set_charset('utf8');
+
+    $name = 'peter';
+    $tel = '123'; 
+    $birthday = '1999-06-04';
+    
+    $sql = 'INSERT INTO cust (name, tel, birthday) VALUES (?,?,?)';
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('sss',$name,$tel,$birthday);
+    $stmt->execute();
+
+
+
+?>
+<!-- $stmt = $mysqli->prepare($sql);  prepare完之後 -->
